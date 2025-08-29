56d3ab4b37721dad1c1f08ec8adf999bd47c0dd5
Eduardo Augusto Coleta de Souza <ecoza@192.168.0.223>
2025-08-28 21:19:10 -0300

    Add Dark Theme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar>
       <v-app-bar-title>Azure Service Bus Explorer</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="toggleTheme">
+        <v-icon>mdi-theme-light-dark</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container fluid>
@@ -12,7 +16,14 @@
 </template>
 
 <script setup lang="ts">
+import { useTheme } from 'vuetify'
 import ServiceBusExplorer from './components/ServiceBusExplorer.vue'
+
+const theme = useTheme()
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
 </script>
 
 <style>
